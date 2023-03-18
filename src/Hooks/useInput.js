758c333e79dc9246b4fdfc310cd55cfa1b94640a
Sethu-bharathi <bharathisethu018@gmail.com>
2023-03-18 11:29:56 +0530

    chat debug v1.0.0

---
+++ src/Hooks/useInput.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function useInput(initialState) {
+  const [state, setState] = useState(initialState);
+
+  function handleChange(e) {
+    setState(e.target.value);
+  }
+
+  function resetState() {
+    setState("");
+  }
+
+  return [state, handleChange, resetState];
+}
+
+export default useInput;
